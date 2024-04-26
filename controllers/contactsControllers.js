a8525f392d380cd5b824a44e27e80fe5c476f22c
NZAlona <amakoveychenko@gmail.com>
2024-04-26 19:16:41 +0300

    Adds half of completed tasks

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,10 +1,47 @@
 import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await contactsService.listContacts();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+    //to reduce code duplication(the below line) we can pass as third parameter next(error)to hanle errors by Express
+    // res.status(500).json({ message: "Server error" });
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactsService.getContactById(id);
 
-export const deleteContact = (req, res) => {};
+    if (!contact) {
+      throw HttpError(404, "Not Found");
+      //   return res.status(404).json({ message: "Not found" });
+    }
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedContact = await contactsService.removeContact(id);
+
+    if (!deletedContact) {
+      throw HttpError(404, "Not Found");
+    }
+
+    res.json(deletedContact);
+  } catch (error) {
+    console.log("hhh");
+    next(error);
+  }
+};
 
 export const createContact = (req, res) => {};
 
---
+++ services/contactsServices.js
@@ -0,0 +1,71 @@
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import crypto from "node:crypto";
+
+const contactsPath = path.resolve("db", "contacts.json");
+// This path.resolve() method accepts string as a sequence of paths into an absolute path
+
+async function readContacts() {
+  const contacts = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  return JSON.parse(contacts);
+  // converts to array of objects
+}
+
+async function writeContacts(contacts) {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+  // converts to json format(string)
+}
+
+async function listContacts() {
+  const arrListOfContacts = await readContacts();
+
+  return arrListOfContacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await readContacts();
+
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  if (typeof contact === "undefined") {
+    return null;
+  }
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readContacts();
+
+  const indexOfcontact = contacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (indexOfcontact === -1) {
+    return null;
+  }
+
+  const removedContact = contacts[indexOfcontact];
+
+  contacts.splice(indexOfcontact, 1);
+  //  method splice mutates  arr of objects(contacts)
+
+  await writeContacts(contacts);
+  // then we rewrite  arr of objects
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await readContacts();
+
+  const newContact = { name, email, phone, id: crypto.randomUUID() };
+  contacts.push(newContact);
+
+  await writeContacts(contacts);
+  return newContact;
+}
+
+export default {
+  listContacts,
+  addContact,
+  removeContact,
+  getContactById,
+};
